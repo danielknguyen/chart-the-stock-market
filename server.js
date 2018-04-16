784ba1df8b7ce287385f510d77bcc221ecb507db
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-04-15 18:47:58 -0700

    added server/app configs

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    dotenv = require('dotenv').config(),
+    bodyParser = require('body-parser'),
+    engines = require('consolidate'),
+    app = express(),
+    morgan = require('morgan');
+
+var appConfig = function() {
+  app.use(express.static(__dirname + '/public'));
+  app.set('views', __dirname + '/views');
+  app.engine('html', engines.nunjucks);
+  app.use(bodyParser.urlencoded({ extended: true}));
+  app.use(bodyParser.json());
+  app.use(morgan('dev'));
+}();
+
+var routes = require('./routes/routes.js')(app);
+var port = process.env.PORT || 27017;
+var server = app.listen(port, function() {
+  console.log("Express server is listening on port %s.", port);
+});
