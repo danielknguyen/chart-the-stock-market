53aad96c3a250688729ec16b0db1ff1b847bbb3b
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-04-27 19:29:02 -0700

    websocket connection

--- server.js
+++ server.js
@@ -5,19 +5,23 @@ var express = require('express'),
     app = express(),
     morgan = require('morgan'),
     yahooFinance = require('yahoo-finance');
+    socket = require('socket.io');
 
 var appConfig = function() {
   app.use(express.static(__dirname + '/public'));
   app.set('views', __dirname + '/views');
   app.engine('html', engines.nunjucks);
-  app.use(bodyParser.urlencoded({ extended: true}));
+  app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json());
   app.use(morgan('dev'));
 }();
-// var db = require('./libs/db.js');
-var Stock = require('./models/stockSchema.js');
-var routes = require('./routes/routes.js')(app, yahooFinance, Stock);
+
 var port = process.env.PORT || 27017;
 var server = app.listen(port, function() {
   console.log("Express server is listening on port %s.", port);
 });
+
+var io = socket(server);
+
+var Stock = require('./models/stockSchema.js');
+var routes = require('./routes/routes.js')(app, yahooFinance, Stock, io, socket);
